8344dd65e723de106a2d562b2334ed92de3f4bab
Willbecomecoder <singhshivani41055@gmail.com>
2026-06-17 15:33:12 +0530

    add sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,26 +5,6 @@ export default function sitemap(): MetadataRoute.Sitemap {
     {
       url: 'https://www.aarqotech.com',
       lastModified: new Date(),
-      changeFrequency: 'weekly',
-      priority: 1,
-    },
-    {
-      url: 'https://www.aarqotech.com/about',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://www.aarqotech.com/services',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: 'https://www.aarqotech.com/contact',
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.7,
     },
   ]
 }
